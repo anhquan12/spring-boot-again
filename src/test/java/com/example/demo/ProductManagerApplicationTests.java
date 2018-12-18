f0dd59d0f0545578777c6db3d4e9634bbc811f7d
Anh Quan <anhquan@Anhs-MacBook-Pro.local>
2018-12-18 23:53:52 +0700

    junit testing

--- src/test/java/com/example/demo/ProductManagerApplicationTests.java
+++ src/test/java/com/example/demo/ProductManagerApplicationTests.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
+import com.example.demo.domain.Product;
+import com.example.demo.service.ProductService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,26 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ProductManagerApplicationTests {
 
+    @Autowired
+    private ProductService productService;
+
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void findProductById() {
+        Product product = productService.findById(1);
+        System.out.println(product);
+    }
+
+    @Test
+    public void insertProduct() { productService.save(new Product("c",2));
+    }
+
+    @Test
+    public void deleteProduct() {
+        productService.delete(1);
     }
 
 }
